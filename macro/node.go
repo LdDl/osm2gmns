ff7f429e96b36ba6baf9b5caf7e2182a8c65c75b
LdDl <sexykdi@gmail.com>
2024-12-12 19:21:51 +0300

    redundant field

--- macro/node.go
+++ macro/node.go
@@ -28,30 +28,25 @@ type Node struct {
 	geom             orb.Point
 	geomEuclidean    orb.Point
 
-	/* Mesoscopic */
-	movements        []*movement.Movement
-	movementIsNeeded bool
-
 	/* Not used */
 	isCentroid bool
 }
 
 func NewNodeFrom(id gmns.NodeID, node *wrappers.NodeOSM) *Node {
 	newNode := Node{
-		incomingLinks:    make([]gmns.LinkID, 0),
-		outcomingLinks:   make([]gmns.LinkID, 0),
-		activityType:     types.ACTIVITY_NONE,
-		name:             node.Name,
-		osmHighway:       node.OsmData.Highway,
-		ID:               id,
-		osmNodeID:        node.ID,
-		intersectionID:   -1,
-		zoneID:           -1,
-		poiID:            -1,
-		controlType:      node.ControlType,
-		boundaryType:     types.BOUNDARY_NONE,
-		geom:             node.InnerNode.Point(),
-		movementIsNeeded: true, // Consider all nodes as intersections by default
+		incomingLinks:  make([]gmns.LinkID, 0),
+		outcomingLinks: make([]gmns.LinkID, 0),
+		activityType:   types.ACTIVITY_NONE,
+		name:           node.Name,
+		osmHighway:     node.OsmData.Highway,
+		ID:             id,
+		osmNodeID:      node.ID,
+		intersectionID: -1,
+		zoneID:         -1,
+		poiID:          -1,
+		controlType:    node.ControlType,
+		boundaryType:   types.BOUNDARY_NONE,
+		geom:           node.InnerNode.Point(),
 	}
 	newNode.geomEuclidean = geomath.PointToEuclidean(newNode.geom)
 	return &newNode
