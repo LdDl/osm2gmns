1a196b22e45e339e54a5ef53ce965a61e42b062a
Dimitrii Lopanov <sexykdi@gmail.com>
2025-03-21 15:16:14 +0300

    multipart handler

--- osm_read.go
+++ osm_read.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 	"time"
@@ -122,6 +123,115 @@ func ReadOSMFromFile(file *os.File, parserType ParserType, allowedAgentTypes []t
 			scannerNodes = osmxml.New(context.Background(), file)
 		case PARSER_PBF:
 			scannerNodes = osmpbf.New(context.Background(), file, 4)
+		default:
+			return nil, fmt.Errorf("file extension '%s' is not handled yet", parserType)
+		}
+		defer scannerNodes.Close()
+
+		// Scan nodes
+		for scannerNodes.Scan() {
+			obj := scannerNodes.Object()
+			if obj.ObjectID().Type() != "node" {
+				continue
+			}
+			node := obj.(*osm.Node)
+			// if _, ok := nodesSeen[node.ID]; ok {
+			// delete(nodesSeen, node.ID)
+			preparedNode := wrappers.NewNodeOSMFrom(node)
+			nodes[node.ID] = preparedNode
+			// }
+		}
+		err = scannerNodes.Err()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if VERBOSE {
+		log.Info().Str("scope", "osm_read").Float64("elapsed", time.Since(st).Seconds()).Msg("Processing nodes done!")
+	}
+
+	if VERBOSE {
+		log.Info().Str("scope", "osm_read").Int("ways_num", len(ways)).Msg("")
+		log.Info().Str("scope", "osm_read").Int("nodes_num", len(nodes)).Msg("")
+	}
+
+	osmData := &OSMWaysNodes{
+		ways:              ways,
+		nodes:             nodes,
+		allowedAgentTypes: make([]types.AgentType, len(allowedAgentTypes)),
+	}
+	copy(osmData.allowedAgentTypes, allowedAgentTypes)
+	return osmData, nil
+}
+
+func ReadOSMFromFileMultipart(bufReader multipart.File, parserType ParserType, allowedAgentTypes []types.AgentType) (*OSMWaysNodes, error) {
+	/* Process ways */
+	if VERBOSE {
+		log.Info().Str("scope", "osm_read").Msg("Processing ways")
+	}
+	st := time.Now()
+
+	ways := []*wrappers.WayOSM{}
+	nodesSeen := make(map[osm.NodeID]struct{})
+	{
+		var scannerWays OSMScanner
+		// Guess file extension and prepare correct scanner for ways
+		switch parserType {
+		case PARSER_XML:
+			scannerWays = osmxml.New(context.Background(), bufReader)
+		case PARSER_PBF:
+			scannerWays = osmpbf.New(context.Background(), bufReader, 4)
+		default:
+			return nil, fmt.Errorf("file extension '%s' is not handled yet", parserType)
+		}
+		defer scannerWays.Close()
+
+		// Scan ways
+		for scannerWays.Scan() {
+			obj := scannerWays.Object()
+			if obj.ObjectID().Type() != "way" {
+				continue
+			}
+			way := obj.(*osm.Way)
+			preparedWay := wrappers.NewWayOSMFrom(way)
+			// Mark way's nodes as seen to remove isolated nodes in further
+			for _, node := range way.Nodes {
+				nodesSeen[node.ID] = struct{}{}
+			}
+			ways = append(ways, preparedWay)
+		}
+		err := scannerWays.Err()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if VERBOSE {
+		log.Info().Str("scope", "osm_read").Float64("elapsed", time.Since(st).Seconds()).Msg("Processing ways done!")
+	}
+	// Seek file to start
+	_, err := bufReader.Seek(0, io.SeekStart)
+	if err != nil {
+		return nil, errors.Wrap(err, "Can't repeat seeking after ways scanning")
+	}
+
+	/* Process nodes */
+	if VERBOSE {
+		log.Info().Str("scope", "osm_read").Msg("Processing nodes")
+	}
+	st = time.Now()
+	nodes := make(map[osm.NodeID]*wrappers.NodeOSM)
+	{
+
+		var scannerNodes OSMScanner
+
+		// Guess file extension and prepare correct scanner for ways
+		switch parserType {
+		case PARSER_XML:
+			scannerNodes = osmxml.New(context.Background(), bufReader)
+		case PARSER_PBF:
+			scannerNodes = osmpbf.New(context.Background(), bufReader, 4)
 		default:
 			return nil, fmt.Errorf("file extension '%s' is not handled yet", parserType)
 		}
